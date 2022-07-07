1e396a2fb704e2da515a3b2fd35548ff447a5a1b
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-07-07 15:08:40 +0200

    feat: opentelemetry support
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- go.mod
+++ go.mod
@@ -6,10 +6,16 @@ require (
 	github.com/roadrunner-server/api/v2 v2.18.0
 	github.com/roadrunner-server/errors v1.1.2
 	github.com/roadrunner-server/sdk/v2 v2.17.3
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.32.0
+	go.opentelemetry.io/contrib/propagators/jaeger v1.7.0
+	go.opentelemetry.io/otel v1.7.0
 	go.opentelemetry.io/otel/trace v1.7.0
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/roadrunner-server/tcplisten v1.1.2 // indirect
-	go.opentelemetry.io/otel v1.7.0 // indirect
+	go.opentelemetry.io/otel/metric v0.30.0 // indirect
 )
--- plugin.go
+++ plugin.go
@@ -8,6 +8,10 @@ import (
 	"github.com/roadrunner-server/api/v2/plugins/config"
 	"github.com/roadrunner-server/errors"
 	"github.com/roadrunner-server/sdk/v2/utils"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	jprop "go.opentelemetry.io/contrib/propagators/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -21,11 +25,13 @@ const (
 type Plugin struct {
 	// server configuration (location, forbidden files and etc)
 	cfg *Config
+
+	prop propagation.TextMapPropagator
 }
 
 // Init must return configure service and return true if service hasStatus enabled. Must return error in case of
 // misconfiguration. Services must not be used without proper configuration pushed first.
-func (s *Plugin) Init(cfg config.Configurer) error {
+func (p *Plugin) Init(cfg config.Configurer) error {
 	const op = errors.Op("headers_plugin_init")
 
 	if !cfg.Has(RootPluginName) {
@@ -36,104 +42,111 @@ func (s *Plugin) Init(cfg config.Configurer) error {
 		return errors.E(op, errors.Disabled)
 	}
 
-	err := cfg.UnmarshalKey(fmt.Sprintf("%s.%s", RootPluginName, PluginName), &s.cfg)
+	err := cfg.UnmarshalKey(fmt.Sprintf("%s.%s", RootPluginName, PluginName), &p.cfg)
 	if err != nil {
 		return errors.E(op, err)
 	}
+
+	p.prop = propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}, jprop.Jaeger{})
 
 	return nil
 }
 
 // Middleware is HTTP plugin middleware to serve headers
-func (s *Plugin) Middleware(next http.Handler) http.Handler {
+func (p *Plugin) Middleware(next http.Handler) http.Handler {
 	// Define the http.HandlerFunc
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if val, ok := r.Context().Value(utils.OtelTracerNameKey).(string); ok {
 			tp := trace.SpanFromContext(r.Context()).TracerProvider()
-			ctx, span := tp.Tracer(val).Start(r.Context(), PluginName)
+			ctx, span := tp.Tracer(val, trace.WithSchemaURL(semconv.SchemaURL),
+				trace.WithInstrumentationVersion(otelhttp.SemVersion())).
+				Start(r.Context(), PluginName, trace.WithSpanKind(trace.SpanKindServer))
 			defer span.End()
+
+			// inject
+			p.prop.Inject(ctx, propagation.HeaderCarrier(r.Header))
 			r = r.WithContext(ctx)
 		}
 
-		if s.cfg.Request != nil {
-			for k, v := range s.cfg.Request {
+		if p.cfg.Request != nil {
+			for k, v := range p.cfg.Request {
 				r.Header.Add(k, v)
 			}
 		}
 
-		if s.cfg.Response != nil {
-			for k, v := range s.cfg.Response {
+		if p.cfg.Response != nil {
+			for k, v := range p.cfg.Response {
 				w.Header().Set(k, v)
 			}
 		}
 
-		if s.cfg.CORS != nil {
+		if p.cfg.CORS != nil {
 			if r.Method == http.MethodOptions {
-				s.preflightRequest(w)
+				p.preflightRequest(w)
 
 				return
 			}
-			s.corsHeaders(w)
+			p.corsHeaders(w)
 		}
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (s *Plugin) Name() string {
+func (p *Plugin) Name() string {
 	return PluginName
 }
 
 // configure OPTIONS response
-func (s *Plugin) preflightRequest(w http.ResponseWriter) {
+func (p *Plugin) preflightRequest(w http.ResponseWriter) {
 	headers := w.Header()
 
 	headers.Add("Vary", "Origin")
 	headers.Add("Vary", "Access-Control-Request-Method")
 	headers.Add("Vary", "Access-Control-Request-Headers")
 
-	if s.cfg.CORS.AllowedOrigin != "" {
-		headers.Set("Access-Control-Allow-Origin", s.cfg.CORS.AllowedOrigin)
+	if p.cfg.CORS.AllowedOrigin != "" {
+		headers.Set("Access-Control-Allow-Origin", p.cfg.CORS.AllowedOrigin)
 	}
 
-	if s.cfg.CORS.AllowedHeaders != "" {
-		headers.Set("Access-Control-Allow-Headers", s.cfg.CORS.AllowedHeaders)
+	if p.cfg.CORS.AllowedHeaders != "" {
+		headers.Set("Access-Control-Allow-Headers", p.cfg.CORS.AllowedHeaders)
 	}
 
-	if s.cfg.CORS.AllowedMethods != "" {
-		headers.Set("Access-Control-Allow-Methods", s.cfg.CORS.AllowedMethods)
+	if p.cfg.CORS.AllowedMethods != "" {
+		headers.Set("Access-Control-Allow-Methods", p.cfg.CORS.AllowedMethods)
 	}
 
-	if s.cfg.CORS.AllowCredentials != nil {
-		headers.Set("Access-Control-Allow-Credentials", strconv.FormatBool(*s.cfg.CORS.AllowCredentials))
+	if p.cfg.CORS.AllowCredentials != nil {
+		headers.Set("Access-Control-Allow-Credentials", strconv.FormatBool(*p.cfg.CORS.AllowCredentials))
 	}
 
-	if s.cfg.CORS.MaxAge > 0 {
-		headers.Set("Access-Control-Max-Age", strconv.Itoa(s.cfg.CORS.MaxAge))
+	if p.cfg.CORS.MaxAge > 0 {
+		headers.Set("Access-Control-Max-Age", strconv.Itoa(p.cfg.CORS.MaxAge))
 	}
 
 	w.WriteHeader(http.StatusOK)
 }
 
 // configure CORS headers
-func (s *Plugin) corsHeaders(w http.ResponseWriter) {
+func (p *Plugin) corsHeaders(w http.ResponseWriter) {
 	headers := w.Header()
 
 	headers.Add("Vary", "Origin")
 
-	if s.cfg.CORS.AllowedOrigin != "" {
-		headers.Set("Access-Control-Allow-Origin", s.cfg.CORS.AllowedOrigin)
+	if p.cfg.CORS.AllowedOrigin != "" {
+		headers.Set("Access-Control-Allow-Origin", p.cfg.CORS.AllowedOrigin)
 	}
 
-	if s.cfg.CORS.AllowedHeaders != "" {
-		headers.Set("Access-Control-Allow-Headers", s.cfg.CORS.AllowedHeaders)
+	if p.cfg.CORS.AllowedHeaders != "" {
+		headers.Set("Access-Control-Allow-Headers", p.cfg.CORS.AllowedHeaders)
 	}
 
-	if s.cfg.CORS.ExposedHeaders != "" {
-		headers.Set("Access-Control-Expose-Headers", s.cfg.CORS.ExposedHeaders)
+	if p.cfg.CORS.ExposedHeaders != "" {
+		headers.Set("Access-Control-Expose-Headers", p.cfg.CORS.ExposedHeaders)
 	}
 
-	if s.cfg.CORS.AllowCredentials != nil {
-		headers.Set("Access-Control-Allow-Credentials", strconv.FormatBool(*s.cfg.CORS.AllowCredentials))
+	if p.cfg.CORS.AllowCredentials != nil {
+		headers.Set("Access-Control-Allow-Credentials", strconv.FormatBool(*p.cfg.CORS.AllowCredentials))
 	}
 }
